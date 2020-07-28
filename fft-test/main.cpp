9ef38088a86c073ef301e9b2ab3bf79388f9f565
StefanLober <stefan.lober@outlook.de>
2020-07-28 11:07:57 +0200

    fix: vector instead of unique_ptr Makefile renamed

--- fft-test/main.cpp
+++ fft-test/main.cpp
@@ -6,15 +6,15 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <memory>
 #include <cmath>
 #include <chrono>
 #include <thread>
+#include <vector>
 
 using namespace std;
 
 const auto processor_count = thread::hardware_concurrency();
-const int DATA_COUNT = 0x10000;
+const int DATA_COUNT = 0x1000;
 
 const int LENGTH = 0x4000;
 const int CUT_OFF = 0x80;
@@ -34,27 +34,27 @@ void fft(ThreadData* threadData)
 
 int main()
 {
-    unique_ptr<thread> threads(new thread[processor_count]);
-    unique_ptr<ThreadData> threadDataPtr(new ThreadData[processor_count]);
+    vector<thread> threads(processor_count);
+    vector<ThreadData> threadData(processor_count);
     
     chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
     for(unsigned int i=0; i<processor_count; i++)
     {
-        threadDataPtr.get()[i].input = new double[LENGTH];
-        threadDataPtr.get()[i].output = new double[LENGTH + 1];
+        threadData[i].input = new double[LENGTH];
+        threadData[i].output = new double[LENGTH + 1];
 
         for (int x = 0; x < LENGTH; x++)
         {
-            threadDataPtr.get()[i].input[x] = (x < LENGTH / 2 ? 1 : 0) + sin(x * (double)2 * M_PI / (double)LENGTH);
+            threadData[i].input[x] = (x < LENGTH / 2 ? 1 : 0) + sin(x * (double)2 * M_PI / (double)LENGTH);
         }
 
-        threads.get()[i] = thread(fft, threadDataPtr.get());
+        threads[i] = thread(fft, &threadData[i]);
     }
 
     for (unsigned int i = 0; i < processor_count; i++)
     {
-        threads.get()[i].join();
+        threads[i].join();
     }
 
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
@@ -63,9 +63,9 @@ int main()
     ofstream resultFile("result.csv");
     for (int x = 0; x < LENGTH; x++)
     {
-        resultFile << threadDataPtr.get()[0].input[x] << ";";
+        resultFile << threadData[0].input[x] << ";";
 
-        auto firstThreadDataOutput = threadDataPtr.get()[0].output;
+        auto firstThreadDataOutput = threadData[0].output;
         if (x * 2 < CUT_OFF)
             resultFile << sqrt(firstThreadDataOutput[x * 2] * firstThreadDataOutput[x * 2] + firstThreadDataOutput[x * 2 + 1] * firstThreadDataOutput[x * 2 + 1]) * 2 / (LENGTH) << endl;
         else
