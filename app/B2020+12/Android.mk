5c0b5769445fe94196b3e50d8be2a77bedb78af8
StefanLober <stefan.lober@outlook.de>
2020-11-21 09:37:14 +0100

    make files for app moved to app dir

--- app/B2020+12/Android.mk
+++ app/B2020+12/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := ooura
-LOCAL_SRC_FILES := ooura/ooura.cpp
+LOCAL_SRC_FILES := ../../ooura/ooura.cpp
 
 LOCAL_CFLAGS    += -O3
 LOCAL_CPPFLAGS  += -O3
